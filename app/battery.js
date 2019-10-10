e2ecfb6c2e3e2cd6dfba78bf8916c9f730e62f4b
Kristin Kuche <kristin.kuche@gmail.com>
2019-10-10 13:31:24 +0200

    Remove unused code

--- app/battery.js
+++ app/battery.js
@@ -4,8 +4,6 @@ import { battery } from "power";
 export let bat_low = document.getElementById("bat-image-2");
 
 export let root = document.getElementById('root')
-export const screenHeight = root.height //250 - Ionic, 300 - Versa
-export const screenWidth = root.width
 
 export function drawBat() {
   let level = battery.chargeLevel;
